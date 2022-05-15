ed99e3afadb83a40f720e925e957919b18b4a69a
Rudeluy <55767063+Rudeluy@users.noreply.github.com>
2022-05-15 19:56:22 -0400

    Update mayor de 3.rb

--- 1.Flujo/Desf4 - El mayor de tres numeros/mayor de 3.rb
+++ 1.Flujo/Desf4 - El mayor de tres numeros/mayor de 3.rb
@@ -2,9 +2,9 @@ nro_1 = ARGV[0].to_i
 nro_2 = ARGV[1].to_i
 nro_3 = ARGV[2].to_i
 if nro_1 > nro_2 && nro_1 > nro_3
-    puts "El numero mayor es #{nro_1}"
+    puts nro_1
 elsif nro_2 > nro_1 && nro_2 > nro_3
-    puts "El numero mayor es #{nro_2}"
+    puts nro_2
 else 
-    puts "El numero mayor es #{nro_3}"
+    puts nro_3
 end
