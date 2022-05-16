3951076874cde70e4d9d72baaf88383ea43ac471
Rudeluy <55767063+Rudeluy@users.noreply.github.com>
2022-05-15 21:06:11 -0400

    se agrega Prueba mayor de 4

---
+++ 1.Flujo/Desf5 - El mayor de cuatro numeros/mayor_de_4.rb
@@ -0,0 +1,21 @@
+nro_1 = ARGV[0].to_i
+nro_2 = ARGV[1].to_i
+nro_3 = ARGV[2].to_i
+nro_4 = ARGV[3].to_i
+if nro_4 == 0
+    if nro_1 >= nro_2 && nro_1 >= nro_3
+        puts nro_1
+    elsif nro_2 >= nro_1 && nro_2 >= nro_3
+        puts nro_2
+    else 
+        puts nro_3
+    end
+elsif nro_1 >= nro_2 && nro_1 >= nro_3 && nro_1 >= nro_4
+    puts nro_1
+elsif nro_2 >= nro_1 && nro_2 >= nro_3 && nro_2 >= nro_4
+    puts nro_2
+elsif nro_3 >= nro_1 && nro_3 >= nro_2 && nro_3 >= nro_4
+    puts nro_3
+else
+    puts nro_4
+end
